4edec80652a7a90e68b82b25e765872bfa48e486
djtomkoya <djtomkoya@gmail.com>
2025-07-07 18:25:37 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+ window.firebaseConfig = {
+    apiKey: "AIzaSyAVTSM4cqKjvNqpJXV1qadRwTSQiAJHSzM",
+    authDomain: "test-timer-ad669.firebaseapp.com",
+    projectId: "test-timer-ad669",
+    storageBucket: "test-timer-ad669.firebasestorage.app",
+    messagingSenderId: "800709028008",
+    appId: "1:800709028008:web:c59c73e6625b015d6b3a9d",
+    measurementId: "G-GJ7CH5WQ2J"
+  };
